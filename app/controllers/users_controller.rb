7d296a36b53c52ecebc71de31b40fccc289691dd
Elise Bernal <elisebernal.o@gmail.com>
2022-06-01 12:15:23 +0200

    user en cours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :description)
+    params.require(:user).permit(:name, :description, :photo)
   end
 end
