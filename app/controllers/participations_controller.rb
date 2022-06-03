d570ae930f8dbe3df5871d8639d78017ac7bec09
justine gauthier <gauthier.justine25000@gmail.com>
2022-06-03 13:13:06 +0200

    j'espère que ça marche

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -7,30 +7,48 @@ class ParticipationsController < ApplicationController
     @user = User.find(params[:user_id])
     @challenge = Challenge.find(params[:challenge_id])
     @participation = Participation.new(user: @user, challenge: @challenge)
-    duration = @challenge.total_time
     if @participation.save
-      case @challenge.frequency
-      when "Quotidien"
-        date = @challenge.start_date - 1
-        while duration.positive?
-          new_date = date += 1
-          Progression.new(date: new_date, participation: @participation, points: 10)
-          duration -= 1
-          Progression.save
-        end
-      when "Hebdomadaire"
-        date = @challenge.start_date - 7
-        while duration.positive?
-          new_date = date += 7
-          Progression.new(date: new_date, participation: @participation, points: 50)
-          duration -= 7
-          Progression.save
-        end
-      when "Unique"
-        new_date = @challenge.end_date
-        Progression.new(date: new_date, participation: @participation, points: 150)
-        Progression.save
-      end
+      create_progressions(@challenge, @participation)
     end
+  end
+
+  private
+
+  def create_progressions(challenge, participation)
+    case challenge.frequency
+    when "Quotidien"
+      daily_progressions(challenge, participation)
+    when "Hebdomadaire"
+      weekly_progression(challenge, participation)
+    when "Unique"
+      monthly_progression(challenge, participation)
+    end
+  end
+
+  def daily_progression(challenge, participation)
+    duration = challenge.total_time
+    date = challenge.start_date - 1
+    while duration.positive?
+      new_date = date += 1
+      Progression.new(date: new_date, participation: participation, points: 10)
+      duration -= 1
+      Progression.save
+    end
+  end
+
+  def weekly_progression(challenge, participation)
+    date = challenge.start_date - 7
+    while duration.positive?
+      new_date = date += 7
+      Progression.new(date: new_date, participation: participation, points: 50)
+      duration -= 7
+      Progression.save
+    end
+  end
+
+  def monthly_progression(challenge, participation)
+    new_date = challenge.end_date
+    Progression.new(date: new_date, participation: participation, points: 150)
+    Progression.save
   end
 end
